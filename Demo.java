3a7df84d882ebb776e0e0dc2e312c12bcce50209
nathan <1136026744@qq.com>
2020-06-07 22:23:13 +0800

    first commit

---
+++ Demo.java
@@ -0,0 +1,402 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Demo {
+    public static class IODemo{
+        public static void pathDemo(){
+            var f = new File(".\\temp.txt");
+            System.out.println(f.getPath());
+            System.out.println(f.getAbsolutePath());
+            try{
+                System.out.println(f.getCanonicalPath());
+            }catch (IOException e){
+                System.out.println(e.toString());
+                return;
+            }
+            System.out.println(f.isFile());
+            System.out.println(f.isDirectory());
+            try{
+                if(f.createNewFile()){
+                    System.out.print("create");
+                    var t = new Scanner(System.in);
+                    String input = t.nextLine();
+                    if(f.delete()) {
+                        System.out.print("delete");
+                        t.nextLine();
+                    }
+                }
+            }catch (IOException e){
+                System.out.println(e.toString());
+                return;
+            }
+            try{
+                File t = File.createTempFile("temp","txt");
+            }catch (IOException e){
+                System.out.println(e.toString());
+                return;
+            }
+            File l = new File("..");
+            File[] fl = l.listFiles();
+            System.out.println(Arrays.toString(fl));
+
+            Path p1 = Paths.get(".", "project", "study"); // 构造一个Path对象
+            System.out.println(p1);
+            Path p2 = p1.toAbsolutePath(); // 转换为绝对路径
+            System.out.println(p2);
+            Path p3 = p2.normalize(); // 转换为规范路径
+            System.out.println(p3);
+            File nf = p3.toFile(); // 转换为File对象
+            System.out.println(f);
+            for (Path p : Paths.get("..").toAbsolutePath()) { // 可以直接遍历Path
+                System.out.println("  " + p);
+            }
+        }
+
+        public static void inputStreamDemo(){
+            int t, len = 0;
+            try(InputStream in = new FileInputStream("README.md");) {
+                byte[] buffer = new byte[1000];
+                while((t = in.read(buffer)) != -1){
+                    if(t > 0) len = t;
+                    System.out.println(String.format("read %d bytes", t));
+                }
+                String res = new String(Arrays.copyOf(buffer,len));
+                System.out.println(res);
+            }catch (IOException e){
+                System.out.println(e.toString());
+                return;
+            }
+
+            byte[] data = { 72, 101, 108, 108, 111, 33 };
+            try (InputStream input = new ByteArrayInputStream(data)) {
+                int n;
+                while ((n = input.read()) != -1) {
+                    System.out.print((char)n);
+                }
+                System.out.println();
+            }catch (IOException e){
+                System.out.println(e.toString());
+            }
+        }
+
+        public static void outputStreamDemo(String s){
+            var f = new File(".\\temp.txt");
+            boolean have_create = true;
+            try{
+                if (!f.exists()) {have_create = f.createNewFile();}
+            }catch (IOException ie){
+                System.out.println(ie.toString());
+                return;
+            }
+            if(f.exists()){
+                try(OutputStream out = new FileOutputStream(f.getAbsolutePath())){
+                    out.write(s.getBytes(StandardCharsets.UTF_8));
+                }catch (IOException ie){
+                    System.out.println(ie.toString());
+                }
+            }
+        }
+
+        public static void readerDemo() {
+            try(Reader r = new FileReader(".\\README.md", StandardCharsets.UTF_8)){
+                char[] buffer = new char[60];
+                int n;
+                while((n = r.read(buffer)) != -1){
+                    System.out.println(String.format("read %d chars", n));
+                }
+                String res = new String(buffer);
+                System.out.println(res);
+            }catch (IOException ie){
+                System.out.println(ie.toString());
+                return;
+            }
+
+            try(Reader t = new InputStreamReader(System.in)) {
+                int i = 0;
+            }catch (IOException ie){
+                System.out.println(ie.toString());
+                return;
+            }
+
+            try (Reader reader = new CharArrayReader("Hello".toCharArray())) {
+                System.out.println("simulate char array file");
+            }catch (IOException ie){
+                System.out.println(ie.toString());
+                return;
+            }
+
+            try (Reader reader = new StringReader("Hello")) {
+                System.out.println("simulate string file");
+            }catch (IOException ie){
+                System.out.println(ie.toString());
+            }
+        }
+
+        public static void writerDemo(String s){
+            try(Writer r = new FileWriter(".\\temp.txt", StandardCharsets.UTF_8,false)){
+                r.write(s); // overwrite
+            }catch (IOException ie){
+                System.out.println(ie.toString());
+                return;
+            }
+
+            CharArrayWriter writer = new CharArrayWriter();
+            writer.write(65);
+            writer.write(66);
+            writer.write(67);
+            char[] data = writer.toCharArray(); // { 'A', 'B', 'C' }
+        }
+
+        public static void printWriterDemo(String[] ss) {
+            try(var pw = new PrintWriter(new FileWriter("temp.txt", StandardCharsets.UTF_8))){
+                for(var s : ss) pw.println(s);
+            }catch (IOException ie){
+                System.out.println(ie.toString());
+            }
+        }
+    }
+
+    public static class ThreadDemo{
+        public static void createThreadDemo(){
+            var t1 = new OneThread();
+            t1.start();
+            var t2 = new Thread(new OneRunnableClass());
+            t2.start();
+            var t3 = new Thread(()->{
+                System.out.println("start");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ignored) {}
+                System.out.println("end");
+            });
+            t3.start();
+            var t4 = new Thread(){
+                public void run(){
+                    System.out.println("start ".concat(this.toString()));
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException ignored) {}
+                    System.out.println("end ".concat(this.toString()));
+                }
+            };
+            t4.start();
+        }
+        static class OneThread extends Thread{
+            @Override public void run(){
+                System.out.println("start ".concat(this.toString()));
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException ignored) {}
+                System.out.println("end ".concat(this.toString()));
+            }
+        }
+        static class OneRunnableClass implements Runnable{
+            @Override public void run(){
+                System.out.println("start ".concat(this.toString()));
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ignored) {}
+                System.out.println("end ".concat(this.toString()));
+            }
+        }
+
+        public static void interruptedThreadDemo(){
+            var t1 = new InterruptableThread();
+            t1.start();
+            try{
+                Thread.sleep(1);
+            }catch (InterruptedException ignore){}
+            t1.interrupt();
+            try{
+                t1.join();
+            }catch (InterruptedException ignore){}
+            System.out.println("end");
+
+        }
+        static class InterruptableThread extends Thread{
+            int n = 0;
+            @Override public void run(){
+                while(!interrupted()){
+                    n++;
+                    System.out.println(n);
+                }
+            }
+        }
+
+        public static void volatileDemo(){
+            HelloThread t = new HelloThread();
+            t.start();
+            try{
+                Thread.sleep(1);
+            }catch (InterruptedException ignore){}
+            t.running = false; // 标志位置为false
+        }
+        static class HelloThread extends Thread {
+            public volatile boolean running = true;
+            public void run() {
+                int n = 0;
+                while (running) {
+                    n ++;
+                    System.out.println(n + " hello!");
+                }
+                System.out.println("end!");
+            }
+        }
+
+        public static void daemonDemo(){
+            Thread t = new TimerThread();
+            t.setDaemon(true);
+            t.start();
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException ignore){}
+        }
+        static class TimerThread extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(LocalTime.now());
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static void synchronizeDemo(){
+            var add = new AddThread();
+            var dec = new DecThread();
+            add.start();
+            dec.start();
+            try{
+                add.join();
+                dec.join();
+            }catch (InterruptedException ignore){}
+            System.out.println(Counter.count);
+        }
+        static class Counter {
+            public static final Object lock = new Object();
+            public static int count = 0;
+        }
+        static class AddThread extends Thread {
+            public void run() {
+                for (int i=0; i<1000; i++) {
+                    synchronized(Counter.lock) {
+                        Counter.count += 1;
+                    }
+                }
+            }
+        }
+        static class DecThread extends Thread {
+            public void run() {
+                for (int i=0; i<2000; i++) {
+                    synchronized(Counter.lock) {
+                        Counter.count -= 1;
+                    }
+                }
+            }
+        }
+
+        public static void join() throws InterruptedException {
+            Thread t = new Thread(){
+                public void run(){
+                    System.out.println("Thread start.");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {}
+                    System.out.println("Thread end.");
+                }
+            };
+            Thread tt = new Thread(){
+                public void run(){
+                    System.out.println("Thread start.");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {}
+                    System.out.println("Thread end.");
+                }
+            };
+            t.start();
+            t.join();
+            tt.start();
+            tt.join();
+        }
+        public static void syn() throws InterruptedException {
+            Object lock = new Object();
+            final int[] c = {0};
+            var add = new Thread(){
+                public void run(){
+                    for(int i = 0; i < 10000; i++){
+                        synchronized(lock){
+                            c[0]++;
+                        }
+                    }
+                }
+            };
+            var dec = new Thread(){
+                public void run(){
+                    for(int i = 0; i < 10000; i++){
+                        synchronized(lock){
+                            c[0]--;
+                        }
+                    }
+                }
+            };
+            add.start();
+            dec.start();
+            add.join();
+            dec.join();
+            System.out.println(c[0]);
+        }
+
+    }
+
+    public static class StreamDemo {
+        private static final Map<String, String> phone = new HashMap<String, String>() {{
+            put("2", "abc");
+            put("3", "def");
+            put("4", "ghi");
+            put("5", "jkl");
+            put("6", "mno");
+            put("7", "pqrs");
+            put("8", "tuv");
+            put("9", "wxyz");
+        }};
+        public static List<String> letterCombinations(String digits) {
+            if(digits.equals("")) return List.of();
+            int len = digits.length();
+            List<String> res = null;
+            Stream<String> s1 = Stream.of(digits);
+            for(int i = 0; i < len; i++){
+                int finalI = i;
+                Stream<List<String>> temp = s1.parallel().map(
+                        (str) -> {
+                            String chars = phone.get(String.valueOf(str.charAt(finalI)));
+                            int chars_len = chars.length();
+                            String start = str.substring(0, finalI);
+                            String end = str.substring(finalI+1, len);
+                            String[] t = new String[chars_len];
+                            for(int j = 0; j < chars_len; j++){
+                                t[j] = start.concat(String.valueOf(chars.charAt(j))).concat(end);
+                            }
+                            return List.of(t);
+                        }
+                );
+                Stream<String> s2 = temp.parallel().flatMap(Collection::stream);
+                res = s2.parallel().collect(Collectors.toList());
+                if(i < len - 1)
+                    s1 = res.parallelStream();
+            }
+            return res;
+        }
+    }
+}
+
---
+++ LeetCode.java
@@ -0,0 +1,196 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class LeetCode {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+        ListNode(){}
+    }
+    //#7
+    // reverse int with restriction
+    // 123 ---> 321
+    public static int reverse(int x){
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+    //#19
+    // remove the nth node of reverse order
+    // 1->2->3->4, 2 ---> 1->2->4
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        boolean is_len_one = (head.next == null);
+        int head_order = RecursiveRemoveNthFromEnd(head, n);
+        if(is_len_one & n == 1) return null;
+        else if(head_order == n) return head.next;
+        else return head;
+    }
+    private static int RecursiveRemoveNthFromEnd(ListNode node, int n){
+        if(node.next == null) return 1;
+        int this_order = RecursiveRemoveNthFromEnd(node.next, n) + 1;
+        if(this_order == n + 1) node.next = node.next.next;
+        return this_order;
+    }
+    //#22
+    //  k pairs parenthesis permutation
+    // 3 ---> ["((()))", "(()())", "(())()", "()(())", "()()()"]
+    public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        StringBuilder init = new StringBuilder("(");
+        RecursiveGenerateParenthesis(init, 1, 1, 1, res, n);
+        return res;
+    }
+    private static void RecursiveGenerateParenthesis(StringBuilder strb, int str_len, int left_count, int stack_ptr, List<String> res, int n){
+        if(str_len == (2*n)) res.add(strb.toString());
+        else{
+            str_len++;
+            if(stack_ptr >= 1 & stack_ptr <= n-1 & left_count <= n-1){
+                StringBuilder t = new StringBuilder(strb);
+                strb.append("(");
+                t.append(")");
+                RecursiveGenerateParenthesis(strb, str_len, left_count+1, stack_ptr+1, res, n);
+                RecursiveGenerateParenthesis(t, str_len, left_count, stack_ptr-1, res, n);
+            }else if(stack_ptr == 0 & left_count <= n-1){
+                strb.append("(");
+                RecursiveGenerateParenthesis(strb, str_len, left_count+1, stack_ptr+1, res, n);
+            }else{
+                strb.append(")");
+                RecursiveGenerateParenthesis(strb, str_len, left_count, stack_ptr-1, res, n);
+            }
+        }
+    }
+    //#23
+    // merge k sorted lists
+    //[[1 4 6] [2 3 5]]  ---> [1 2 3 4 5 6]
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) return null;
+        ListNode[] lst = new ListNode[lists.length];
+        int heap_size = lst.length;
+        int idx = 0;
+        for(var i : lists){
+            if(i != null){
+                lst[idx++] = i;
+            }else{
+                heap_size--;
+            }
+        }
+        if(heap_size == 0) return null;
+        buildMinHeap(lst, heap_size);
+        ListNode head;
+        ListNode head_ptr;
+        head = lst[0];
+        head_ptr = head;
+        lst[0] = lst[0].next;
+        if(lst[0] == null){
+            lst[0] = lst[heap_size - 1];
+            heap_size--;
+            minHeapify(lst, 0, heap_size);
+        }
+        while(heap_size > 0){
+            minHeapify(lst, 0, heap_size);
+            head_ptr.next = lst[0];
+            head_ptr = head_ptr.next;
+            lst[0] = lst[0].next;
+            if(lst[0] == null){
+                lst[0] = lst[heap_size - 1];
+                heap_size--;
+                minHeapify(lst, 0, heap_size);
+            }
+        }
+        return head;
+    }
+    private static void minHeapify(ListNode[] arr, int idx, int heap_size){
+        int l = 2 * (idx + 1);
+        int l_idx = l - 1;
+        int r = 2 * (idx + 1) + 1;
+        int r_idx = r - 1;
+        int min_idx = idx;
+        if ((l_idx < heap_size) && (arr[l_idx].val < arr[min_idx].val)) {
+            min_idx = l_idx;
+        }
+        if ((r_idx < heap_size) && (arr[r_idx].val < arr[min_idx].val)) {
+            min_idx = r_idx;
+        }
+        if (min_idx != idx) {
+            var t = arr[min_idx];
+            arr[min_idx] = arr[idx];
+            arr[idx] = t;
+            minHeapify(arr, min_idx, heap_size);
+        }
+    }
+    private static void buildMinHeap(ListNode[] arr, int heap_size){
+        for(int i = heap_size/2 - 1; i >= 0; i--){
+            minHeapify(arr, i, heap_size);
+        }
+    }
+    //#25
+    // reverse a linked list every k elements
+    //1->2->3->4->5->6, 3  --->  3->2->1->6->5->4
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null | k <= 1) return head;
+        ListNode res = new ListNode();
+        ListNode handle;
+        res.next = head;
+        handle = res;
+        while(hasKChildren(handle, k)) handle = reverseGroup(handle, k);
+        return res.next;
+    }
+    private static ListNode reverseGroup(ListNode handle, int k){
+        // have at least k node
+        ListNode rest, ptr1, ptr2, head;
+        head = handle.next;
+        ptr2 = head;
+        ptr1 = head.next;
+        rest = ptr1.next;
+        // iterate k - 1 times
+        for(int i = 0; i < k - 2; i++){
+            ptr1.next = ptr2;
+            ptr2 = ptr1;
+            ptr1 = rest;
+            rest = rest.next;
+        }
+        ptr1.next = ptr2;
+        ptr2 = ptr1;
+        //
+        head.next = rest;
+        handle.next = ptr2;
+        return head;
+    }
+    private static boolean hasKChildren(ListNode handle, int k){
+        for(int i = 0; i < k; i++){
+            handle = handle.next;
+            if(handle == null) return false;
+        }
+        return true;
+    }
+    //#560
+    // find the count of continue sub-arrays which sum is k
+    //[1, 2, 3, 4], 3 ---> 2    ([1, 2] and [3])
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap< Integer, Integer > mp = new HashMap < > ();
+        mp.put(0, 1);
+        for (int num : nums) {
+            pre += num;
+            if (mp.containsKey(pre - k))
+                count += mp.get(pre - k);
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+    //
+
+}
