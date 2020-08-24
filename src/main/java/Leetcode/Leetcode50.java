0cd3ea0a2d7545528abfef48ee8fe53501b1ab8d
nathan <1136026744@qq.com>
2020-08-24 10:55:55 +0800

    review

--- src/main/java/Leetcode/Leetcode50.java
+++ src/main/java/Leetcode/Leetcode50.java
@@ -1075,6 +1075,12 @@ public class Leetcode50 {
             }
         }
 
+        spaces.sort((m1,m2)->{
+            var rc1 = relatedSpaces.get(m1).size();
+            var rc2 = relatedSpaces.get(m2).size();
+            return rc1 - rc2;
+        });
+
         Map<MatrixIndex, HashSet<Character>> availableChars = new HashMap<>();
         for (var space : spaces) {
             availableChars.put(space, getAvailableChars(space, board));
