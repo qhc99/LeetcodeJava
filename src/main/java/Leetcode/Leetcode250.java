03d9ab37446cb21ed7632aa0251f324b46577962
nathan <1136026744@qq.com>
2020-09-27 18:27:22 +0800

    add new solution

---
+++ src/main/java/Leetcode/Leetcode250.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+public class Leetcode250 {
+
+    /**
+     * #235
+     * <br/>二叉搜索树的最近公共祖先
+     * @param root tree
+     * @param p node 1
+     * @param q node 2
+     * @return closest ancestor
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p,q);
+        }
+        else{
+            return root;
+        }
+    }
+}
