916dcdc7cb4bad47da2c72495217db8b49eac987
nathan <1136026744@qq.com>
2020-09-27 18:17:17 +0800

    add TODO

--- src/main/java/Leetcode/Leetcode550.java
+++ src/main/java/Leetcode/Leetcode550.java
@@ -1,7 +1,58 @@
 package Leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode550
 {
+    /**
+     * #501
+     * <br/>二叉搜索树中的众数
+     * @param root tree
+     * @return res
+     */
+    @SuppressWarnings("unused")
+    public int[] findMode(TreeNode root) {
+        var s = new Statistic();
+        dfs(root, s);
+        int[] mode = new int[s.answer.size()];
+        for (int i = 0; i < s.answer.size(); ++i) {
+            mode[i] = s.answer.get(i);
+        }
+        return mode;
+    }
+
+    public void dfs(TreeNode o, Statistic solver) {
+        if (o == null) {
+            return;
+        }
+        dfs(o.left, solver);
+        solver.update(o.val);
+        dfs(o.right,solver);
+    }
+
+    static class Statistic {
+        List<Integer> answer = new ArrayList<>();
+        int base, count, maxCount;
+
+        public void update(int x) {
+            if (x == base) {
+                ++count;
+            } else {
+                count = 1;
+                base = x;
+            }
+            if (count == maxCount) {
+                answer.add(base);
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                answer.clear();
+                answer.add(base);
+            }
+        }
+    }
+
     /**
      * #538
      * <br/>二叉搜索树转换为累加树
