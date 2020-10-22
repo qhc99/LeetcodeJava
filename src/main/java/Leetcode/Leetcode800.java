8dc408461ffcdb825e4dc86d24d931abc42b2dd0
nathan <1136026744@qq.com>
2020-10-22 20:15:48 +0800

    add

--- src/main/java/Leetcode/Leetcode800.java
+++ src/main/java/Leetcode/Leetcode800.java
@@ -1,12 +1,44 @@
 package Leetcode;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 
 public class Leetcode800 {
+
+    /**
+     * #763
+     * <br/>划分字母区间
+     * <pre>
+     * 输入：S = "ababcbacadefegdehijhklij"
+     * 输出：[9,7,8]
+     * 解释：
+     * 划分结果为 "ababcbaca", "defegde", "hijhklij"。
+     * 每个字母最多出现在一个片段中。
+     * 像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/partition-labels
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * </pre>
+     * @param S string
+     * @return res
+     */
+    public static List<Integer> partitionLabels(String S) {
+        int[] lastIndexOfChar = new int[26];
+        for(int i = 0; i < S.length(); i++){
+            lastIndexOfChar[S.charAt(i) - 'a'] = i;
+        }
+        int startIdx = 0, endIndex = 0;
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < S.length(); i++){
+            endIndex = Math.max(lastIndexOfChar[S.charAt(i) - 'a'], endIndex);
+            if(i == endIndex){
+                res.add(endIndex - startIdx + 1);
+                startIdx = endIndex + 1;
+            }
+        }
+        return res;
+    }
 
     /**
      * #785
