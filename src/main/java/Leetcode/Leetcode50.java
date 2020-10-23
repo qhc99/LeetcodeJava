b0c586f8f16616cb9e9ec4867a52fc680771b4ec
nathan <1136026744@qq.com>
2020-10-23 15:20:25 +0800

    add

--- src/main/java/Leetcode/Leetcode50.java
+++ src/main/java/Leetcode/Leetcode50.java
@@ -1724,5 +1724,28 @@ public class Leetcode50 {
         }
     }
 
-
+    /**
+     * #48
+     * <br/>旋转图像
+     * @param matrix image
+     */
+    public static void rotate(int[][] matrix) {
+        if(matrix.length <= 1){
+            return;
+        }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i + 1; j < matrix.length; j++){
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix.length/2; j++){
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = t;
+            }
+        }
+    }
 }
