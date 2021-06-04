52c2255e71944d05b439b70c343fbaff2e98dc21
NathanDrakeHat <1136026744@qq.com>
2021-06-04 09:33:27 +0800

    add solution

--- src/main/java/Leetcode/Leetcode200.java
+++ src/main/java/Leetcode/Leetcode200.java
@@ -23,6 +23,24 @@ public class Leetcode200{
     }
 
     /**
+     * #160 相交链表
+     * @param headA linked list
+     * @param headB linked list
+     * @return intersect
+     */
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+    /**
      * #173
      */
     public static class BSTIterator {
