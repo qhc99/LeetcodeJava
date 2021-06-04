1e0406247711ad787ff341533d41504bf4f5b424
NathanDrakeHat <1136026744@qq.com>
2021-06-04 10:26:46 +0800

    add

--- src/main/java/Leetcode/Leetcode550.java
+++ src/main/java/Leetcode/Leetcode550.java
@@ -3,7 +3,7 @@ package Leetcode;
 import java.util.*;
 import java.util.stream.IntStream;
 
-public class Leetcode550 {
+public class Leetcode550{
     /**
      * #501
      * <br/>二叉搜索树中的众数
@@ -12,18 +12,18 @@ public class Leetcode550 {
      * @return res
      */
     @SuppressWarnings("unused")
-    public static int[] findMode(TreeNode root) {
+    public static int[] findMode(TreeNode root){
         var s = new Statistic();
         dfs(root, s);
         int[] mode = new int[s.answer.size()];
-        for (int i = 0; i < s.answer.size(); ++i) {
+        for(int i = 0; i < s.answer.size(); ++i){
             mode[i] = s.answer.get(i);
         }
         return mode;
     }
 
-    public static void dfs(TreeNode o, Statistic solver) {
-        if (o == null) {
+    public static void dfs(TreeNode o, Statistic solver){
+        if(o == null){
             return;
         }
         dfs(o.left, solver);
@@ -31,22 +31,22 @@ public class Leetcode550 {
         dfs(o.right, solver);
     }
 
-    static class Statistic {
+    static class Statistic{
         List<Integer> answer = new ArrayList<>();
         int base, count, maxCount;
 
-        public void update(int x) {
-            if (x == base) {
+        public void update(int x){
+            if(x == base){
                 ++count;
             }
-            else {
+            else{
                 count = 1;
                 base = x;
             }
-            if (count == maxCount) {
+            if(count == maxCount){
                 answer.add(base);
             }
-            if (count > maxCount) {
+            if(count > maxCount){
                 maxCount = count;
                 answer.clear();
                 answer.add(base);
@@ -58,27 +58,37 @@ public class Leetcode550 {
      * #525
      * <br/> 连续数组
      * <br/>给定一个二进制数组 nums , 找到含有相同数量的 0 和 1 的最长连续子数组，并返回该子数组的长度。
+     *
      * @param nums nums
      * @return len
      */
-    public static int findMaxLength(int[] nums) {
+    public static int findMaxLength(int[] nums){
         int len = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
         int max = 0;
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i == 0; i++){
             if(nums[i] == 0){
                 nums[i] = -1;
-            }
-            if(i > 0){
-                nums[i] += nums[i-1];
-            }
-            if(nums[i] == 0){
-                max = Math.max(i+1, max);
             }
             if(!map.containsKey(nums[i])){
                 map.put(nums[i], i);
             }
-            else {
+            else{
+                max = Math.max(i - map.get(nums[i]), max);
+            }
+        }
+        for(int i = 1; i < len; i++){
+            if(nums[i] == 0){
+                nums[i] = -1;
+            }
+            nums[i] += nums[i - 1];
+            if(nums[i] == 0){
+                max = Math.max(i + 1, max);
+            }
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], i);
+            }
+            else{
                 max = Math.max(i - map.get(nums[i]), max);
             }
         }
@@ -106,13 +116,13 @@ public class Leetcode550 {
      * @return converted tree
      */
     @SuppressWarnings("unused")
-    public static TreeNode convertBST(TreeNode root) {
+    public static TreeNode convertBST(TreeNode root){
         traverseAndConvertBST(root, 0);
         return root;
     }
 
-    private static int traverseAndConvertBST(TreeNode r, int sum) {
-        if (r == null) {
+    private static int traverseAndConvertBST(TreeNode r, int sum){
+        if(r == null){
             return sum;
         }
         sum = traverseAndConvertBST(r.right, sum);
