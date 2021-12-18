2881e0b4d0a2edf5a3afa217c79b06b4921cfc45
NathanDrakeHat <1136026744@qq.com>
2021-12-18 16:35:14 -0500

    save

--- src/main/java/Leetcode/Leetcode250.java
+++ src/main/java/Leetcode/Leetcode250.java
@@ -158,7 +158,6 @@ public class Leetcode250 {
    * @return res
    */
   public static boolean isPalindrome(ListNode head) {
-    //TODO O(1) memory
     List<Integer> vals = new ArrayList<>();
 
     // 将链表的值复制到数组中
