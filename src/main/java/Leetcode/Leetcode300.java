17336b444f6953a26290866e485456d64c593c19
NathanDrakeHat <1136026744@qq.com>
2021-12-29 15:44:50 -0500

    save

--- src/main/java/Leetcode/Leetcode300.java
+++ src/main/java/Leetcode/Leetcode300.java
@@ -1,35 +1,73 @@
 package Leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("JavaDoc")
 public class Leetcode300 {
+
   /**
-   * #328
-   * @param head
+   * #257
+   *
+   * @param root
    * @return
    */
-  public static ListNode oddEvenList(ListNode head) {
-    if(head == null){
-      return null;
-    }
-    ListNode oddTail = head, evenHead, evenTail;
-    if(head.next == null){
-      return head;
-    }
-    ListNode ptr = head.next;
-    evenHead = ptr;
-    evenTail = ptr;
-    ptr = ptr.next;
-    for(int i = 3; ptr != null; i++, ptr = ptr.next){
-      if(i % 2 == 1){
-        oddTail.next = ptr;
-        oddTail = ptr;
+  public static List<String> binaryTreePaths(TreeNode root) {
+    StringBuilder stringStack = new StringBuilder();
+    List<String> ans = new ArrayList<>();
+    var recurFunc = new Object() {
+      void apply(TreeNode node) {
+        var idx = stringStack.length();
+        if (node.left == null && node.right == null) {
+          if (stringStack.length() == 0) stringStack.append(node.val);
+          else stringStack.append("->").append(node.val);
+          ans.add(stringStack.toString());
+          stringStack.delete(idx, stringStack.length());
+        }
+        else {
+          if (node.left != null) {
+            if (stringStack.length() == 0) stringStack.append(node.val);
+            else stringStack.append("->").append(node.val);
+            apply(node.left);
+            stringStack.delete(idx, stringStack.length());
+          }
+          if (node.right != null) {
+            if (stringStack.length() == 0) stringStack.append(node.val);
+            else stringStack.append("->").append(node.val);
+            apply(node.right);
+            stringStack.delete(idx, stringStack.length());
+          }
+        }
       }
-      else {
-        evenTail.next = ptr;
-        evenTail = ptr;
+
+    };
+    recurFunc.apply(root);
+    return ans;
+  }
+
+  /**
+   * #274
+   *
+   * @param citations
+   * @return
+   */
+  public static int hIndex(int[] citations) {
+    Arrays.sort(citations);
+    int n = citations.length;
+    int s = 0, e = citations.length + 1;
+    while (e - s > 1) {
+      int mid = (s + e) / 2;
+      if(mid == 0){
+        if(citations[citations.length - 1] <= mid) s = mid;
+        else e = mid;
+      }
+      else{
+        if(citations[n - mid] >= mid) s = mid;
+        else e = mid;
       }
     }
-    oddTail.next = evenHead;
-    evenTail.next = null;
-    return head;
+    return s;
+
   }
 }
--- src/main/java/Leetcode/Leetcode350.java
+++ src/main/java/Leetcode/Leetcode350.java
@@ -3,7 +3,40 @@ package Leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("JavaDoc")
 public class Leetcode350 {
+
+  /**
+   * #328
+   * @param head
+   * @return
+   */
+  public static ListNode oddEvenList(ListNode head) {
+    if(head == null){
+      return null;
+    }
+    ListNode oddTail = head, evenHead, evenTail;
+    if(head.next == null){
+      return head;
+    }
+    ListNode ptr = head.next;
+    evenHead = ptr;
+    evenTail = ptr;
+    ptr = ptr.next;
+    for(int i = 3; ptr != null; i++, ptr = ptr.next){
+      if(i % 2 == 1){
+        oddTail.next = ptr;
+        oddTail = ptr;
+      }
+      else {
+        evenTail.next = ptr;
+        evenTail = ptr;
+      }
+    }
+    oddTail.next = evenHead;
+    evenTail.next = null;
+    return head;
+  }
 
   /**
    * #337
