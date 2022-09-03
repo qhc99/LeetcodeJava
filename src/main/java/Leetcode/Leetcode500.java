c161dcec865d5f23cb27af9a35e57c7b237fdb33
qhc <onlinecqh99@gmail.com>
2022-09-03 18:56:31 -0400

    add new leetcode answers

--- src/main/java/Leetcode/Leetcode500.java
+++ src/main/java/Leetcode/Leetcode500.java
@@ -344,6 +344,44 @@ public class Leetcode500 {
   }
 
   /**
+   * #474
+   *
+   * @param strs
+   * @param m
+   * @param n
+   * @return
+   */
+  public static int findMaxForm(String[] strs, int m, int n) {
+    int[][] dp = new int[m + 1][n + 1];
+    for (var s : strs) {
+      var count = countZerosAndOnes(s);
+      for (int i = m; i >= 0; i--) {
+        for(int j = n; j >= 0; j--){
+          if(i >= count[0] && j >= count[1]) {
+            dp[i][j] = Math.max(dp[i][j],dp[i-count[0]][j-count[1]] + 1);
+          }
+        }
+      }
+    }
+    int max = -1;
+    for(var a : dp){
+      for(var num : a){
+        max = Math.max(num,max);
+      }
+    }
+    return max;
+  }
+
+  private static int[] countZerosAndOnes(String s) {
+    int[] ans = new int[2];
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '0') ans[0]++;
+      else ans[1]++;
+    }
+    return ans;
+  }
+
+  /**
    * #475
    *
    * @param houses
