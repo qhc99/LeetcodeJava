6e7f4bf669740ef2793415a6b02ac955ba702179
qhc <onlinecqh99@gmail.com>
2022-10-25 18:09:45 -0400

    add new leetcode answers

---
+++ src/main/java/Leetcode/Leetcode1700.java
@@ -0,0 +1,55 @@
+package Leetcode;
+
+import java.util.Iterator;
+
+@SuppressWarnings({"unused", "JavadocDeclaration"})
+public class Leetcode1700 {
+  /**
+   * #1662
+   * @param word1
+   * @param word2
+   * @return
+   */
+  public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+    var iter1 = new IterStrArr(word1);
+    var iter2 = new IterStrArr(word2);
+    while (iter1.hasNext()){
+      if(iter2.hasNext()){
+        var i1 = iter1.next();
+        var i2 = iter2.next();
+        if(!i1.equals(i2)) return false;
+      }
+      else return false;
+    }
+    return !iter2.hasNext();
+  }
+
+  private static class IterStrArr implements Iterator<Character> {
+    final String[] words;
+    int arr_idx = 0;
+    int word_idx = -1;
+    IterStrArr(String[] arr){
+      words = arr;
+    }
+
+    @Override
+    public boolean hasNext() {
+      if(word_idx + 1 < words[arr_idx].length()){
+        return true;
+      }
+      else {
+        return arr_idx + 1 < words.length;
+      }
+    }
+
+    @Override
+    public Character next() {
+      word_idx++;
+      if(word_idx >= words[arr_idx].length()){
+        word_idx = 0;
+        arr_idx++;
+      }
+      return words[arr_idx].charAt(word_idx);
+    }
+  }
+}
