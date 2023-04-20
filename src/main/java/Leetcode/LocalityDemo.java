f168ddf3c764c21e3dd53be150c8fdc7f3486579
qhc <onlinecqh99@gmail.com>
2023-04-20 18:57:35 -0400

    save

--- src/main/java/Leetcode/LocalityDemo.java
+++ src/main/java/Leetcode/LocalityDemo.java
@@ -43,16 +43,9 @@ public class LocalityDemo {
         }
     }
 
-    public static void runDemo() {
+    public static void reset_mat(float[][] m1, float[][] m2) {
         Random r = new Random();
-
-        int size_a = 1300;
-        int size_b = 1300;
-        int size_c = 1300;
-        float[][] m1 = new float[size_a][size_b];
-        float[][] m2 = new float[size_b][size_c];
-        float[][] m3 = new float[size_a][size_c];
-
+        int size_a = m1.length, size_b = m2.length, size_c = m2[0].length;
         for (int i = 0; i < size_a; i++) {
             for (int j = 0; j < size_b; j++) {
                 m1[i][j] = r.nextFloat();
@@ -63,15 +56,49 @@ public class LocalityDemo {
                 m2[i][j] = r.nextFloat();
             }
         }
+    }
 
-        for (int i = 0; i < size_a; i++) {
-            for (int j = 0; j < size_c; j++) {
-                m3[i][j] = 0;
+    public static void space_locality(float[][] m1, float[][] m2, float[][] m3) {
+        int w_s = 64;
+        int size_a = m1.length, size_b = m2.length, size_c = m2[0].length;
+        MatPartition p1 = new MatPartition(m1, w_s);
+        MatPartition p2 = new MatPartition(m2, w_s);
+        MatPartition p3 = new MatPartition(m3, w_s);
+        var t1 = System.nanoTime();
+
+        for (int i = 0; i < p3.rows; i++) {
+            for (int j = 0; j < p3.cols; j++) {
+                MatBlock m3r = p3.at(i, j);
+                for (int p = 0; p + m3r.r1 < m3r.r2; p++) {
+                    for (int q = 0; q + m3r.c1 < m3r.c2; q++) {
+                        m3[p + m3r.r1][q + m3r.c1] = 0;
+                    }
+                }
+                for (int k = 0; k < p1.cols; k++) {
+                    MatBlock m1r = p1.at(i, k);
+                    MatBlock m2r = p2.at(k, j);
+                    mul_mat_block(m1, m1r, m2, m2r, m3, m3r);
+                }
             }
         }
+        var t2 = System.nanoTime();
 
+        double ans = 0;
+        for (int i = 0; i < size_a; i++) {
+            for (int j = 0; j < size_a; j++) {
+                ans += m3[i][j];
+            }
+        }
+        System.out.println("locality optimization:");
+        System.out.printf("spend %.2fms%n", (t2 - t1) / 1000000.);
+        System.out.printf("sum: %f\n", ans);
+        System.out.println();
+    }
+
+    public static void space_locality_parallel(float[][] m1, float[][] m2, float[][] m3) {
         var t1 = System.nanoTime();
-        int w_s = 32;
+        int w_s = 64;
+        int size_a = m1.length, size_b = m2.length, size_c = m2[0].length;
         MatPartition p1 = new MatPartition(m1, w_s);
         MatPartition p2 = new MatPartition(m2, w_s);
         MatPartition p3 = new MatPartition(m3, w_s);
@@ -103,94 +130,18 @@ public class LocalityDemo {
         System.out.printf("spend %.2fms%n", (t2 - t1) / 1000000.);
         System.out.printf("sum: %f\n", ans);
         System.out.println();
+    }
 
-        for (int i = 0; i < size_a; i++) {
-            for (int j = 0; j < size_b; j++) {
-                m1[i][j] = r.nextFloat();
-            }
-        }
-        for (int i = 0; i < size_b; i++) {
-            for (int j = 0; j < size_c; j++) {
-                m2[i][j] = r.nextFloat();
-            }
-        }
-
-        for (int i = 0; i < size_a; i++) {
-            for (int j = 0; j < size_c; j++) {
-                m3[i][j] = 0;
-            }
-        }
-        t1 = System.nanoTime();
-
-        for(int i = 0; i < p3.rows; i++){
-            for (int j = 0; j < p3.cols; j++) {
-                MatBlock m3r = p3.at(i, j);
-                for (int p = 0; p + m3r.r1 < m3r.r2; p++) {
-                    for (int q = 0; q + m3r.c1 < m3r.c2; q++) {
-                        m3[p + m3r.r1][q + m3r.c1] = 0;
-                    }
-                }
-                for (int k = 0; k < p1.cols; k++) {
-                    MatBlock m1r = p1.at(i, k);
-                    MatBlock m2r = p2.at(k, j);
-                    mul_mat_block(m1, m1r, m2, m2r, m3, m3r);
-                }
-            }
-        }
-        t2 = System.nanoTime();
-
-        ans = 0;
-        for (int i = 0; i < size_a; i++) {
-            for (int j = 0; j < size_a; j++) {
-                ans += m3[i][j];
-            }
-        }
-        System.out.println("locality optimization:");
-        System.out.printf("spend %.2fms%n", (t2 - t1) / 1000000.);
-        System.out.printf("sum: %f\n", ans);
-        System.out.println();
-
-
-
-
-
-        for (int i = 0; i < size_a; i++) {
-            for (int j = 0; j < size_b; j++) {
-                m1[i][j] = r.nextFloat();
-            }
-        }
-        for (int i = 0; i < size_b; i++) {
-            for (int j = 0; j < size_c; j++) {
-                m2[i][j] = r.nextFloat();
-            }
-        }
-
-        for (int i = 0; i < size_a; i++) {
-            for (int j = 0; j < size_c; j++) {
-                m3[i][j] = 0;
-            }
-        }
-        var t3 = System.nanoTime();
-        IntStream.range(0, size_a).parallel().forEach(i -> {
-            for (int j = 0; j < size_c; j++) {
-                m3[i][j] = 0;
-                for (int k = 0; k < size_b; k++) {
-                    m3[i][j] += m1[i][k] * m2[k][j];
-                }
-            }
-        });
-        var t4 = System.nanoTime();
-
-        ans = 0;
-        for (int i = 0; i < size_a; i++) {
-            for (int j = 0; j < size_a; j++) {
-                ans += m3[i][j];
-            }
-        }
-
-
-        System.out.println("parallel implementation:");
-        System.out.printf("spend %.2fms%n", (t4 - t3) / 1000000.);
-        System.out.printf("sum: %f\n", ans);
+    public static void runDemo() {
+        int size_a = 1300;
+        int size_b = 1300;
+        int size_c = 1300;
+        float[][] m1 = new float[size_a][size_b];
+        float[][] m2 = new float[size_b][size_c];
+        float[][] m3 = new float[size_a][size_c];
+        reset_mat(m1, m2);
+        space_locality_parallel(m1, m2, m3);
+        reset_mat(m1, m2);
+        space_locality(m1, m2, m3);
     }
 }
