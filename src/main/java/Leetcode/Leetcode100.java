4f60b4fa7bd3c42a63a4d85f1ddc4d3937f1b986
qhc99 <onlinecqh99@gmail.com>
2026-06-29 16:14:52 -0400

    Add

--- src/main/java/Leetcode/Leetcode100.java
+++ src/main/java/Leetcode/Leetcode100.java
@@ -485,4 +485,30 @@ public class Leetcode100 {
         }
         return G[n];
     }
+
+    /**
+     * #97
+     * 
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return
+     */
+    public boolean isInterleave(String s1, String s2, String s3) {
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        if (s3.length() != s1.length() + s2.length())
+            return false;
+        dp[0][0] = true;
+        for (int i = 0; i < s1.length() + 1; i++) {
+            for (int j = 0; j < s2.length() + 1; j++) {
+                if (i - 1 >= 0 && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                if (j - 1 >= 0 && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+                    dp[i][j] |= dp[i][j - 1];
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
 }
