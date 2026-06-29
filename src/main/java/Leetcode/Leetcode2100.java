1ac9d5113970022c1627a5d1f0217defb021934d
qhc99 <onlinecqh99@gmail.com>
2026-06-29 18:02:36 -0400

    Add

---
+++ src/main/java/Leetcode/Leetcode2100.java
@@ -0,0 +1,79 @@
+package Leetcode;
+
+import java.util.*;
+
+public class Leetcode2100 {
+    /**
+     * #2013 DetectSquares
+     */
+    class DetectSquares {
+
+        static class Point {
+            private final int x;
+            private final int y;
+
+            public Point(int[] data) {
+                if (data == null || data.length != 2) {
+                    throw new IllegalArgumentException(
+                            "Point must have exactly 2 values");
+                }
+                this.x = data[0];
+                this.y = data[1];
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o)
+                    return true;
+                if (!(o instanceof Point other))
+                    return false;
+                return x == other.x && y == other.y;
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(x, y);
+            }
+        }
+
+        Map<Integer, List<int[]>> xAxis = new HashMap<>();
+        Map<Integer, List<int[]>> yAxis = new HashMap<>();
+        Map<Point, Integer> count = new HashMap<>();
+
+        public DetectSquares() {
+
+        }
+
+        public void add(int[] point) {
+            xAxis.computeIfAbsent(point[0], t -> new ArrayList<>()).add(point);
+            yAxis.computeIfAbsent(point[1], t -> new ArrayList<>()).add(point);
+            count.put(new Point(point),
+                    count.getOrDefault(new Point(point), 0) + 1);
+        }
+
+        public int count(int[] point) {
+            var xPoints = xAxis.getOrDefault(point[0], List.of());
+            var yPoints = yAxis.getOrDefault(point[1], List.of());
+            Set<Integer> yOfXPoints = new HashSet<>(
+                    xPoints.stream().map(p -> p[1]).toList());
+            Set<Integer> xOfYPoints = new HashSet<>(
+                    yPoints.stream().map(p -> p[0]).toList());
+            int res = 0;
+            for (var x : xOfYPoints) {
+                for (var y : yOfXPoints) {
+                    if (x != point[0] && Math.abs(x - point[0]) == Math
+                            .abs(y - point[1])) {
+                        var corner1 = count.getOrDefault(
+                                new Point(new int[] { x, point[1] }), 0);
+                        var corner2 = count.getOrDefault(
+                                new Point(new int[] { point[0], y }), 0);
+                        var corner3 = count
+                                .getOrDefault(new Point(new int[] { x, y }), 0);
+                        res += corner1 * corner2 * corner3;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
