dbdee4fcd87ea1810f2a34cb2e61499f761608fa
qhc99 <onlinecqh99@gmail.com>
2026-06-29 14:09:46 -0400

    Add

--- src/main/java/Leetcode/Leetcode100.java
+++ src/main/java/Leetcode/Leetcode100.java
@@ -247,6 +247,34 @@ public class Leetcode100 {
     }
 
     /**
+     * #72
+     * 
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < word1.length() + 1; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < word2.length() + 1; i++) {
+            dp[0][i] = 1;
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                int replace = (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0
+                        : 1) + dp[i - 1][j - 1];
+                int add = dp[i - 1][j] + 1;
+                int del = dp[i][j - 1] + 1;
+                dp[i][j] = Math.min(Math.min(add, del), replace);
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+
+    /**
      * #74 <br/>
      * 搜索二维矩阵
      *
