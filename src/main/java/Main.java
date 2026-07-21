0cf2f7f4199bc20d0e50f6b12b3db11043c2ea90
qhc99 <onlinecqh99@gmail.com>
2026-07-21 17:06:49 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,6 @@ import java.math.*;
 
 public class Main {
     public static void main(String[] args) {
-        var t = new Leetcode2000();
-        // a -> x -> y
-        // -> z
-        // b -> x > y
-        // -> z
-        // -> w
-        t.deleteDuplicateFolder(List.of(List.of("a"), List.of("a", "x"),
-                List.of("a", "x", "y"), List.of("a", "z"), List.of("b"),
-                List.of("b", "x"), List.of("b", "x", "y"), List.of("b", "z"),
-                List.of("b", "w")));
+
     }
 }
