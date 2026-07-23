740b20087e5d71cc52aa374d122d17401d90ea2b
qhc99 <onlinecqh99@gmail.com>
2026-07-23 15:54:53 -0400

    Add

--- src/main/java/Leetcode/Leetcode1200.java
+++ src/main/java/Leetcode/Leetcode1200.java
@@ -1,6 +1,8 @@
 package Leetcode;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntConsumer;
 
 public class Leetcode1200 {
     /**
@@ -138,6 +140,78 @@ public class Leetcode1200 {
 
     static record Transaction(int idx, String name, int time, int amount,
             String city) {
+    }
+
+    /**
+     * #1195 FizzBuzz
+     */
+    class FizzBuzz {
+        private AtomicInteger c;
+        private final int n;
+
+        public FizzBuzz(int n) {
+            this.n = n;
+            this.c = new AtomicInteger(1);
+        }
+
+        // printFizz.run() ouatputs "fizz".
+        public void fizz(Runnable printFizz) throws InterruptedException {
+            int v = c.get();
+            while (v <= n) {
+                var t = v % 3;
+                var f = v % 5;
+                if (t == 0 && f != 0) {
+                    printFizz.run();
+                    c.incrementAndGet();
+                }
+                v = c.get();
+            }
+        }
+
+        // printBuzz.run() outputs "buzz".
+        public void buzz(Runnable printBuzz) throws InterruptedException {
+            int v = c.get();
+            while (v <= n) {
+                var t = v % 3;
+                var f = v % 5;
+                if (t != 0 && f == 0) {
+                    printBuzz.run();
+                    c.incrementAndGet();
+                }
+                v = c.get();
+            }
+        }
+
+        // printFizzBuzz.run() outputs "fizzbuzz".
+        public void fizzbuzz(Runnable printFizzBuzz)
+                throws InterruptedException {
+            int v = c.get();
+            while (v <= n) {
+                var t = v % 3;
+                var f = v % 5;
+                if (t == 0 && f == 0) {
+                    printFizzBuzz.run();
+                    c.incrementAndGet();
+                }
+                v = c.get();
+            }
+        }
+
+        // printNumber.accept(x) outputs "x", where x is an integer.
+        public void number(IntConsumer printNumber)
+                throws InterruptedException {
+            int v = c.get();
+            while (v <= n) {
+                var t = v % 3;
+                var f = v % 5;
+                if (t != 0 && f != 0) {
+                    printNumber.accept(v);
+                    ;
+                    c.incrementAndGet();
+                }
+                v = c.get();
+            }
+        }
     }
 
     /**
