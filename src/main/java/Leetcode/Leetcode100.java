395cd791b9228f9bc9e1e4807b24a1a5bd815911
qhc99 <onlinecqh99@gmail.com>
2026-07-24 16:10:27 -0400

    Add

--- src/main/java/Leetcode/Leetcode100.java
+++ src/main/java/Leetcode/Leetcode100.java
@@ -954,6 +954,37 @@ public class Leetcode100 {
     }
 
     /**
+     * #26
+     * 
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int i = 1;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[j - 1])
+                nums[i++] = nums[j];
+        }
+        return i;
+    }
+
+    /**
+     * #27
+     * 
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val)
+                nums[i++] = nums[j];
+        }
+        return i;
+    }
+
+    /**
      * #29 <br/>
      * int divide without '/', '*', '%'
      *
