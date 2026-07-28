f31d310d60a60a1aace12a594c9089b6cc48652e
qhc99 <onlinecqh99@gmail.com>
2026-07-27 22:03:46 -0400

    Add

--- src/main/java/Leetcode/Leetcode1000.java
+++ src/main/java/Leetcode/Leetcode1000.java
@@ -88,6 +88,56 @@ public class Leetcode1000 {
     }
 
     /**
+     * #090
+     * 
+     * @param board
+     * @return
+     */
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        boolean[][] visited = new boolean[n][n];
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] { board[n - 1][0] == -1 ? 1 : board[n - 1][0], 0 });
+        while (!queue.isEmpty()) {
+            var data = queue.poll();
+            var num = data[0];
+            if (num == n * n)
+                return data[1];
+            boolean jumpFar = false;
+            for (int i = 6; i >= 0; i--) {
+                if (num + i > n * n)
+                    continue;
+                var p = num2pos(num + i, n);
+                if (!visited[p[0]][p[1]]) {
+                    visited[p[0]][p[1]] = true;
+                    if (board[p[0]][p[1]] != -1 || !jumpFar) {
+                        queue.add(new int[] {
+                                board[p[0]][p[1]] != -1 ? board[p[0]][p[1]]
+                                        : num + i,
+                                data[1] + 1 });
+                        jumpFar = true;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    int[] num2pos(int i, int n) {
+        i--;
+        var r = n - 1 - i / n;
+        var c = ((i / n) % 2) == 0 ? i % n : n - 1 - i % n;
+        return new int[] { r, c };
+    }
+
+    int pos2num(int[] p, int n) {
+        int d = 1;
+        d += (n - 1 - p[0]) * n;
+        d += (n - 1 - p[0]) % 2 == 0 ? p[1] : n - 1 - p[1];
+        return d;
+    }
+
+    /**
      * #925
      *
      * @param name  name string
