6e774af39a8349549ec195d5a19f787311f56493
qhc99 <onlinecqh99@gmail.com>
2026-07-29 02:40:22 -0400

    Add

--- src/main/java/Leetcode/Leetcode3000.java
+++ src/main/java/Leetcode/Leetcode3000.java
@@ -8,7 +8,27 @@ public class Leetcode3000 {
      * @return
      */
     public int incremovableSubarrayCount(int[] nums) {
-        
-        return 0;
+        int l = 1;
+        int r = nums.length;
+
+        for (; l < nums.length; l++) {
+            if (nums[l - 1] >= nums[l])
+                break;
+        }
+        l = Math.min(l, r - 1);
+        long res = 0;// [l,r)
+        while (r > 0) {
+            res += l + 1;
+            r--;
+            if (r < 0 || nums[r] >= (r + 1 < nums.length ? nums[r + 1]
+                    : Integer.MAX_VALUE))
+                break;
+            while ((l - 1 >= 0 ? nums[l - 1] : Integer.MIN_VALUE) >= nums[r]
+                    || r - l < 1) {
+                l--;
+            }
+            l++;
+        }
+        return (int) res;
     }
 }
