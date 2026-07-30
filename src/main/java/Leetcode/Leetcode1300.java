891e2b1512c1653671b47fbbbab8cd00dcdb056f
qhc99 <onlinecqh99@gmail.com>
2026-07-30 17:20:36 -0400

    Add

--- src/main/java/Leetcode/Leetcode1300.java
+++ src/main/java/Leetcode/Leetcode1300.java
@@ -66,6 +66,34 @@ public class Leetcode1300 {
     }
 
     /**
+     * #1213
+     * 
+     * @param arr1
+     * @param arr2
+     * @param arr3
+     * @return
+     */
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2,
+            int[] arr3) {
+        List<Integer> res = new ArrayList<>();
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length && k < arr3.length) {
+            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+                res.add(arr1[i]);
+                i++;
+                j++;
+                k++;
+            } else if (arr1[i] <= arr2[j] && arr1[i] <= arr3[k])
+                i++;
+            else if (arr2[j] <= arr1[i] && arr2[j] <= arr3[k])
+                j++;
+            else
+                k++;
+        }
+        return res;
+    }
+
+    /**
      * #1217
      *
      * @param position
--- src/main/java/Leetcode/Leetcode200.java
+++ src/main/java/Leetcode/Leetcode200.java
@@ -1038,6 +1038,39 @@ public class Leetcode200 {
         }
     }
 
+    class Reader4 {
+        int read4(char[] buf4) {
+            return 0;
+        }
+    }
+
+    /**
+     * #157 Solution
+     */
+    public class Solution extends Reader4 {
+        /**
+         * @param buf Destination buffer
+         * @param n   Number of characters to read
+         * @return The number of actual characters read
+         */
+        public int read(char[] buf, int n) {
+            char[] buf4 = new char[4];
+            int len = 0;
+            while (len < n) {
+                var l = read4(buf4);
+                if (l > 0) {
+                    var fill = Math.min(l, n - len);
+                    System.arraycopy(buf4, 0, buf, len, fill);
+                    len += fill;
+                }
+
+                if (l < 4)
+                    break;
+            }
+            return len;
+        }
+    }
+
     /**
      * #160 相交链表
      *
