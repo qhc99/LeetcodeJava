16c3cfc5a04627ba58b65a82b26488e83ac14c45
qhc99 <onlinecqh99@gmail.com>
2026-08-03 15:32:21 -0400

    Add

--- src/main/java/Leetcode/Leetcode3500.java
+++ src/main/java/Leetcode/Leetcode3500.java
@@ -3,6 +3,64 @@ package Leetcode;
 import java.util.*;
 
 public class Leetcode3500 {
+
+    /**
+     * #3408 TaskManager
+     */
+    class TaskManager {
+        static record Task(int id, int userId) {
+        }
+
+        Map<Integer, Integer> task2Priority = new HashMap<>();
+        Map<Integer, Integer> task2user = new HashMap<>();
+        TreeMap<Integer, TreeSet<Integer>> priority2task = new TreeMap<>();
+
+        public TaskManager(List<List<Integer>> tasks) {
+            for (var task : tasks) {
+                add(task.get(0), task.get(1), task.get(2));
+            }
+        }
+
+        public void add(int userId, int taskId, int priority) {
+            task2Priority.put(taskId, priority);
+            task2user.put(taskId, userId);
+            priority2task.computeIfAbsent(priority, k -> new TreeSet<>())
+                    .add(taskId);
+        }
+
+        public void edit(int taskId, int newPriority) {
+            var oldPriority = task2Priority.put(taskId, newPriority);
+            var s = priority2task.get(oldPriority);
+            s.remove(taskId);
+            if (s.isEmpty())
+                priority2task.remove(oldPriority);
+            priority2task.computeIfAbsent(newPriority, k -> new TreeSet<>())
+                    .add(taskId);
+        }
+
+        public void rmv(int taskId) {
+            var priority = task2Priority.remove(taskId);
+            task2user.remove(taskId);
+            var s = priority2task.get(priority);
+            s.remove(taskId);
+            if (s.isEmpty())
+                priority2task.remove(priority);
+        }
+
+        public int execTop() {
+            var e = priority2task.lastEntry();
+            if (e == null)
+                return -1;
+            var task = e.getValue().last();
+            var user = task2user.get(task);
+            rmv(task);
+            return user;
+        }
+    }
+
+    /**
+     * #3484 Spreadsheet
+     */
     class Spreadsheet {
         Map<Integer, Map<Integer, Integer>> mat = new HashMap<>();
 
@@ -34,7 +92,7 @@ public class Leetcode3500 {
         }
 
         int[] parse(String cell) {
-            if(Character.isDigit(cell.charAt(0))){
+            if (Character.isDigit(cell.charAt(0))) {
                 return null;
             }
             var c = cell.charAt(0) - 'A';
