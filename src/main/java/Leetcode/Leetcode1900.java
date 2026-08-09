f00a3ef1d662eb03fdd4ad42f3ac59c05869569c
qhc99 <onlinecqh99@gmail.com>
2026-08-09 17:34:20 -0400

    Add

--- src/main/java/Leetcode/Leetcode1900.java
+++ src/main/java/Leetcode/Leetcode1900.java
@@ -158,6 +158,27 @@ public class Leetcode1900 {
     }
 
     /**
+     * #1860
+     * 
+     * @param memory1
+     * @param memory2
+     * @return
+     */
+    public int[] memLeak(int memory1, int memory2) {
+        int i = 1;
+        for (; true; i++) {
+            if (memory1 >= memory2 && memory1 >= i) {
+                memory1 -= i;
+            } else if (memory1 < memory2 && memory2 >= i) {
+                memory2 -= i;
+            } else
+                break;
+        }
+
+        return new int[] { i, memory1, memory2 };
+    }
+
+    /**
      * #1870
      * 
      * @param dist
