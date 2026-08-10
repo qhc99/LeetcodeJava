81193475aee48fe5c21fc62b7800cc02d047777b
qhc99 <onlinecqh99@gmail.com>
2026-08-10 16:10:05 -0400

    Add

--- src/main/java/Leetcode/Leetcode1100.java
+++ src/main/java/Leetcode/Leetcode1100.java
@@ -507,6 +507,23 @@ public class Leetcode1100 {
         return arr;
     }
 
+    public String gcdOfStrings(String str1, String str2) {
+        if (str1.length() == str2.length()) {
+            if (str1.equals(str2))
+                return str1;
+            else
+                return "";
+        }
+        if (str2.length() > str1.length()) {
+            var t = str1;
+            str1 = str2;
+            str2 = t;
+        }
+        if (!str1.startsWith(str2))
+            return "";
+        return gcdOfStrings(str1.substring(str2.length(), str1.length()), str2);
+    }
+
     /**
      * #1087
      * 
