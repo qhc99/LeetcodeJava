093042c4021a319b783cd5080dde9776ea28d36f
qhc99 <onlinecqh99@gmail.com>
2026-08-10 17:10:06 -0400

    Add

--- src/main/java/Leetcode/Leetcode700.java
+++ src/main/java/Leetcode/Leetcode700.java
@@ -645,6 +645,26 @@ public class Leetcode700 {
     }
 
     /**
+     * #650
+     * 
+     * @param n
+     * @return
+     */
+    public int minSteps(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; i / j >= 2; j++) {
+                if (i % j == 0) {
+                    dp[i] = Math.min(dp[i], dp[j] + i / j);
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    /**
      * #652
      *
      * @param root
