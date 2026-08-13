182ccd59b924e0e73e8e462798eed37f07159543
qhc99 <onlinecqh99@gmail.com>
2026-08-12 23:34:25 -0400

    Add

--- src/main/java/Leetcode/Leetcode1300.java
+++ src/main/java/Leetcode/Leetcode1300.java
@@ -193,6 +193,45 @@ public class Leetcode1300 {
         return res;
     }
 
+    static class CustomFunction {
+        // Returns f(x, y) for any given positive integers x and y.
+        // Note that f(x, y) is increasing with respect to both x and y.
+        // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+        public int f(int x, int y) {
+            return 0;
+        }
+
+    };
+
+    /**
+     * #1237
+     * 
+     * @param customfunction
+     * @param z
+     * @return
+     */
+    public List<List<Integer>> findSolution(CustomFunction customfunction,
+            int z) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int x = 1; x <= 1000; x++) {
+            int l = 1, r = 1000;
+            while (r - l > 0) {
+                int mid = l + (r - l) / 2;
+                if (customfunction.f(x, mid) < z) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+            while (customfunction.f(x, l) == z) {
+                res.add(List.of(x, l));
+                l++;
+            }
+
+        }
+        return res;
+    }
+
     /**
      * #1242
      * 
