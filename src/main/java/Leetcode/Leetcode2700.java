7379a4ac61aa4cfd8229654eaa482dc60eb4a41d
qhc99 <onlinecqh99@gmail.com>
2026-08-18 15:21:44 -0400

    Add

--- src/main/java/Leetcode/Leetcode2700.java
+++ src/main/java/Leetcode/Leetcode2700.java
@@ -3,6 +3,36 @@ package Leetcode;
 import java.util.*;
 
 public class Leetcode2700 {
+
+    /**
+     * #2672
+     * 
+     * @param n
+     * @param queries
+     * @return
+     */
+    public int[] colorTheArray(int n, int[][] queries) {
+        int[] res = new int[queries.length];
+        int[] color = new int[n];
+        int count = 0;
+        for (int i = 0; i < queries.length; i++) {
+            var query = queries[i];
+            int idx = query[0], clr = query[1];
+            int cntBefore = (idx - 1 >= 0 && color[idx] == color[idx - 1]
+                    && color[idx] != 0) ? 1 : 0;
+            cntBefore += (idx + 1 < color.length && color[idx] == color[idx + 1]
+                    && color[idx] != 0) ? 1 : 0;
+            color[idx] = clr;
+            int cntAfter = (idx - 1 >= 0 && color[idx] == color[idx - 1]
+                    && color[idx] != 0) ? 1 : 0;
+            cntAfter += (idx + 1 < color.length && color[idx] == color[idx + 1]
+                    && color[idx] != 0) ? 1 : 0;
+            count = count - cntBefore + cntAfter;
+            res[i] = count;
+        }
+        return res;
+    }
+
     /**
      * #2673
      * 
