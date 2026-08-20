225c26e18bff3469e591ba161fc2e7bfc24110cb
qhc99 <onlinecqh99@gmail.com>
2026-08-19 22:33:08 -0400

    Add

--- src/main/java/Leetcode/Leetcode200.java
+++ src/main/java/Leetcode/Leetcode200.java
@@ -1536,6 +1536,13 @@ public class Leetcode200 {
         return res;
     }
 
+    public int titleToNumber(String columnTitle) {
+        int res = 0;
+        for (var c : columnTitle.toCharArray())
+            res = res * 26 + c - 'A' + 1;
+        return res;
+    }
+
     /**
      * #173
      */
