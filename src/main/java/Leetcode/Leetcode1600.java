edffee59effa147c43962550bd73435d38703a5c
qhc99 <onlinecqh99@gmail.com>
2026-08-26 20:36:38 -0400

    Add

--- src/main/java/Leetcode/Leetcode1600.java
+++ src/main/java/Leetcode/Leetcode1600.java
@@ -184,7 +184,54 @@ public class Leetcode1600 {
      * @return
      */
     public int maxNumEdgesToRemove(int n, int[][] edges) {
-        return 0;
+        var common = new Disjointset(n + 1);
+        Arrays.sort(edges,
+                Comparator.comparingInt(e -> ((int[]) e)[0]).reversed());
+        int count = 0;
+        Disjointset bob = null, alice = null;
+        for (int i = 0; i < edges.length; i++) {
+            var edge = edges[i];
+            if (edge[0] == 3) {
+                if (common.parent(edge[1]) != common.parent(edge[2])) {
+                    count++;
+                    common.union(edge[1], edge[2]);
+                }
+            } else if (edge[0] == 2) {
+                if (bob == null) {
+                    bob = new Disjointset(common);
+                }
+                if (bob.parent(edge[1]) != bob.parent(edge[2])) {
+                    count++;
+                    bob.union(edge[1], edge[2]);
+                }
+            } else {
+                if (alice == null) {
+                    alice = new Disjointset(common);
+                }
+                if (alice.parent(edge[1]) != alice.parent(edge[2])) {
+                    count++;
+                    alice.union(edge[1], edge[2]);
+                }
+            }
+        }
+        if (bob == null) {
+            bob = new Disjointset(common);
+        }
+        if (alice == null) {
+            alice = new Disjointset(common);
+        }
+        int p = bob.parent(1);
+        for (int i = 2; i <= n; i++) {
+            if (bob.parent(i) != p)
+                return -1;
+        }
+
+        p = alice.parent(1);
+        for (int i = 2; i <= n; i++) {
+            if (alice.parent(i) != p)
+                return -1;
+        }
+        return edges.length - count;
     }
 
     /**
