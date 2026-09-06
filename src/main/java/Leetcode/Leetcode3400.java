90c360cf43025e6f22c1ed263f25b81aee84848f
QC <onlinecqh99@gmail.com>
2026-09-06 17:16:07 -0400

    Add

--- src/main/java/Leetcode/Leetcode3400.java
+++ src/main/java/Leetcode/Leetcode3400.java
@@ -1,6 +1,43 @@
 package Leetcode;
 
+import java.util.*;
+
 public class Leetcode3400 {
+
+    /**
+     * #3341
+     * 
+     * @param moveTime
+     * @return
+     */
+    public int minTimeToReach(int[][] moveTime) {
+        int m = moveTime.length, n = moveTime[0].length;
+        if (m == 1 && n == 1)
+            return moveTime[0][0];
+        Queue<Pos> queue = new PriorityQueue<>(
+                Comparator.comparing(p -> p.time));
+        queue.add(new Pos(0, 0, 0));
+        boolean[][] visited = new boolean[m][n];
+        int[] dx = { 0, 0, 1, -1 }, dy = { 1, -1, 0, 0 };
+        while (!queue.isEmpty()) {
+            var p = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int x = p.i + dx[i], y = p.j + dy[i];
+                if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]) {
+                    visited[x][y] = true;
+                    int t = Math.max(moveTime[x][y] + 1, p.time + 1);
+                    if (x == m - 1 && y == n - 1)
+                        return t;
+                    queue.add(new Pos(t, x, y));
+                }
+            }
+        }
+        return -1;
+    }
+
+    static record Pos(int time, int i, int j) {
+    }
+
     /**
      * #3389
      * 
